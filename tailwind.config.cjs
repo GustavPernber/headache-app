13b24f1f637e2e43d3029917edb5f2c0d2fb6d94
Gustav-Pernber-nti-johanneberg <74712621+Gustav-Pernber-nti-johanneberg@users.noreply.github.com>
2022-03-20 20:19:56 +0100

    Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    colors:{
+      appGreen:{
+        100:"47CE8D",
+        200:"26B17F",
+      },
+      appLightGrey:"#D2D2D2",
+      appWhite:"#EBEDF0",
+      appDark:{
+        100:"#294761",
+        200:"#112A40",
+        300:"#0A1825"
+      }, 
+      appPink:"#DF6886",
+      appOrange:"#CD7360"
+    },
+    extend: {},
+  },
+  plugins: [],
+}

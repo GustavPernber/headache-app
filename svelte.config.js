fa27cb72bc08c352cf9b23ff0626c667ffb6c620
Gustav-Pernber-nti-johanneberg <74712621+Gustav-Pernber-nti-johanneberg@users.noreply.github.com>
2022-03-20 20:38:23 +0100

    scss init

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,14 @@
 import adapter from '@sveltejs/adapter-auto';
+// import preprocess from 'svelte/types/compiler/preprocess';
+import preprocessor from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: preprocessor({
+		scss:{
+			prependData:"@import '../scss/main.scss';"
+		}
+	}),
 	kit: {
 		adapter: adapter()
 	}

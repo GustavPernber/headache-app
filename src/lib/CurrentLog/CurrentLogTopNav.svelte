3a359a32aeaf5c39aef7c8c6a553f75b15032329
Gustav-Pernber-nti-johanneberg <74712621+Gustav-Pernber-nti-johanneberg@users.noreply.github.com>
2022-05-03 14:52:52 +0200

    update fix

--- src/lib/CurrentLog/CurrentLogTopNav.svelte
+++ src/lib/CurrentLog/CurrentLogTopNav.svelte
@@ -2,11 +2,11 @@
 
 
 
-<script/>
+</script>
 
 <div>
 
-    <span on:click={} class="material-icons-outlined text-white">
+    <span class="material-icons-outlined text-white">
         highlight_off
     </span>
 

61f6d6f2c62b7709fdf7adb08edb4c1435351ca4
Gustav-Pernber-nti-johanneberg <74712621+Gustav-Pernber-nti-johanneberg@users.noreply.github.com>
2022-05-05 17:42:09 +0200

    main page is scrollable and settings in nav is fixed (#4)

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,10 +5,12 @@
     import BottomNav from '../lib/components/BottomNav.svelte';
     import HomeCard from '../lib/HomeCard/index.svelte';
     import TopNav from '../lib/components/TopNav.svelte';
+import { bind } from 'svelte/internal';
     
     let showCurrentlog = false
     let currentLogUnmount = false
 
+    let yCord
 
     const handleCurrentlog = ()=>{
         
@@ -27,7 +29,7 @@
 </script>
 
 <div class=" min-h-screen w-full bg-appDark-300 z-10">
-    <TopNav>
+    <TopNav {yCord}>
         <h1 class=" text-white text-xl font-semibold">September</h1>
     </TopNav>
 
@@ -35,6 +37,9 @@
 
     <div>
         <h2 class=" text-white">main content</h2>
+        <div class=" h-[60rem]"></div>
+
+
     </div>
 
 
@@ -47,5 +52,7 @@
 </div>
 
 <BottomNav></BottomNav>
+
+<svelte:window bind:scrollY={yCord}></svelte:window>
 
 

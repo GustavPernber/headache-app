6c34b1b6ba851068e5f8392506735309f9f70fc6
Melanie-Lindgren-nti-johanneberg <74713763+Melanie-Lindgren-nti-johanneberg@users.noreply.github.com>
2022-05-15 14:34:39 +0200

    småfix (#10)

--- src/lib/HomeCard/Week.svelte
+++ src/lib/HomeCard/Week.svelte
@@ -1,3 +1,28 @@
-<div class="w-full bg-appDark-100 h-5">
 
+
+<div class="w-full bg-appDark-100 h-5">
+    <!-- {#each as } -->
 </div>
+
+<!-- {#each timeButtons as buttonData (buttonData.value) }
+
+                {#if buttonData.value != 'custom'}
+
+                    <button class="rounded-lg border-2 border-green-100 text-base h-9 w-[6.5rem]" on:click={(e)=> {e.preventDefault(), handleTimeButton(buttonData.value)}} class:selected={buttonData.selected }> 
+                        {buttonData.name}
+                    </button>
+                    
+                {:else}
+                    
+                    {#if buttonData.selected}
+                        <input type="time"  class="rounded-lg border-2 border-green-100 text-base h-9 w-[6.5rem]">    
+
+                        {:else}
+                        <button class="rounded-lg border-2 border-green-100 text-base h-9 w-[6.5rem]" on:click={(e)=> {e.preventDefault(), handleCustomTime(buttonData.value)}} class:selected={buttonData.selected }> 
+                            {buttonData.name}
+                        </button>
+                    {/if}
+
+                {/if}
+                
+            {/each} -->
--- src/lib/HomeCard/index.svelte
+++ src/lib/HomeCard/index.svelte
@@ -1,17 +1,64 @@
 <script>
     import { goto } from '$app/navigation';
 
+    import TopNav from '../components/TopNav.svelte';
     import Week from './Week.svelte'
 
     let day="Today"
     export let handleCurrentlog
+    
+    let currentDate = new Date();
+	
+    let daysList = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+
+
+    //rendera upp en vecka, skapa en array med en vecka
+    //  [
+    //    { month:"september" weeks:[[week] [week]]}
+    //    { month:"september" weeks:[[week] [week]]}
+    //    { month:"september" weeks:[[{day:"sun", date:18}] [week]]}
+    //  ]
+    //
+    function getWeekDay(c, ) {
+        return daysList[c.getDay()];
+    }
+
+    function getMonth(c) {
+        let monthsList = [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"
+        ];
+        return monthsList[c.getMonth()];
+    }
+
 </script>
 
 
 <article class="bg-appDark-200 flex flex-col rounded-b-2xl  overflow-hidden ">
 
+    <TopNav>
+        <h1 class=" text-white text-xl font-semibold">{getMonth(currentDate)}</h1>
+    </TopNav>
 
-    <Week></Week>
+    <Week {getWeekDay}/>
 
     <div class="w-full min-h-[55vh] py-1 gap-9 flex flex-col justify-center items-center">
 

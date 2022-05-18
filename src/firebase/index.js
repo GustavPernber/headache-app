b866bb1c9c6c314ed5faadf89a37893b1767e9d2
Gustav-Pernber-nti-johanneberg <74712621+Gustav-Pernber-nti-johanneberg@users.noreply.github.com>
2022-05-18 14:19:35 +0200

    only shows todays

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -8,7 +8,7 @@ import {
 	GoogleAuthProvider,
 	signInWithPopup,
 	signOut,
-	getAdditionalUserInfo
+	getAdditionalUserInfo,
 } from "firebase/auth";
 import {
 	getFirestore,
@@ -34,53 +34,92 @@ import {
 } from "firebase/storage";
 import { getMessaging, getToken, onMessage } from "firebase/messaging";
 import { getPerformance } from "firebase/performance";
+import moment from "moment";
 
+let db;
 
-
-let db
-
-export function initFirebase(){
-	initializeApp(firebaseConfig)
-	db = getFirestore()
-
+export function initFirebase() {
+	initializeApp(firebaseConfig);
+	db = getFirestore();
 }
 
+export async function getAllCurrentLogs() {
+	const data = {
+		data: [
+			{ date: "2020-3-4, 8:13:12", painLevel: 2 },
+			{ date: "2020-3-4, 16:30:12", painLevel: 9 },
+			{ date: "2020-3-4, 20:13:12", painLevel: 3 },
+			{ date: "2020-3-4, 12:20:12", painLevel: 5 },
+			{ date: "2020-3-4, 16:40:12", painLevel: 10 },
 
-export async function getAllCurrentLogs(){
+			{ date: "2022-5-18, 6:13:12", painLevel: 4 },
+			{ date: "2022-5-18, 19:20:12", painLevel: 9 },
+			{ date: "2022-5-18, 12:30:12", painLevel: 2 },
+			{ date: "2022-5-7, 23:40:12", painLevel: 6 },
+			{ date: "2022-5-7, 19:13:12", painLevel: 1 },
+		],
+	};
 
-	const querySnapshot=await getDocs(collection(db, 'current-logs'))
+	let todaysData = [];
+	let currentSimpleDate=moment().format("YYYY-MM-DD");
 
-	let allLogs=[]
-	querySnapshot.forEach(doc=>{
-		allLogs.push(doc.data())
-	})
+	for (let i = 0; i < data.data.length; i++) {
+		const log = data.data[i];
+
+		let unixTime = moment(log.date).unix();
+
+		let day = moment.unix(unixTime).format("YYYY-MM-DD");
+		if (day===currentSimpleDate) {
+			let tmpObj = {
+				simpleDate: day,
+				unixTime: unixTime,
+				painLevel: log.painLevel,
+			};
+			
+			todaysData.push(tmpObj);
+		}
 
 
-	return allLogs
-}
 
-export async function addCurrentLog({painLevel, unixTime}){
+		//{today:[{simple, unix, painlevel}]}
 
-    try {
-        const docRef=await addDoc(collection(db, 'current-logs'), {
-            painLevel:painLevel,
-            time:unixTime,
-        })
-		
-		console.log('Created document with id:', docRef.id)
-	} catch (error) {
-		console.log(error)
+		//{date: "2022-06-12, 13:14:12", painLevel: 2}
+		//days:[ {date:2022-05-21, dataPoints:[{time:1231441, pain:1}],  } ]
 	}
+	console.log(todaysData);
+	//loop store recorded dates in array, if not there, created new array, otherwise put it in
+	//[time: 1213314, painLevel:1]
 
+	const querySnapshot = await getDocs(collection(db, "current-logs"));
+
+	let allLogs = [];
+	querySnapshot.forEach((doc) => {
+		allLogs.push(doc.data());
+	});
+
+	return allLogs;
+
+	// return newData
 }
 
+export async function addCurrentLog({ painLevel, unixTime }) {
+	try {
+		const docRef = await addDoc(collection(db, "current-logs"), {
+			painLevel: painLevel,
+			time: unixTime,
+		});
+
+		console.log("Created document with id:", docRef.id);
+	} catch (error) {
+		console.log(error);
+	}
+}
 
 // export async function addToDB(text){
 // 	let uid
 // 	authStore.subscribe(user=>{
 // 		uid=user.user.uid || null
-// 	})	
-
+// 	})
 
 // 	try {
 // 		const docRef=await addDoc(collection(db, 'message'), {
@@ -98,7 +137,7 @@ export async function addCurrentLog({painLevel, unixTime}){
 // 	let uid
 // 	authStore.subscribe(user=>{
 // 		uid=user.user.uid || null
-// 	})	
+// 	})
 // 	const docRef=collection(db, 'message')
 // 	const q= query(docRef, where('uid','==',uid))
 // 	const response = await getDocs(q)

ee1c8c557c7669b0aac24d272fe5a372fccf8389
Gustav-Pernber-nti-johanneberg <74712621+Gustav-Pernber-nti-johanneberg@users.noreply.github.com>
2022-05-19 19:13:09 +0200

    cleanup

--- src/lib/Statistics/PainGraph.svelte
+++ src/lib/Statistics/PainGraph.svelte
@@ -11,15 +11,7 @@
 
 		let logPoints={x:[], y:[]}
 		let tmpArray=[]
-		//applicera tidigare puinkter på idag
-		//beräkna regression utifrån alla punkter som är placerade på idag
-
-		//Gå  igenom alla logs
-		//Ta ut timma, minut, sekund. Skapa DAte.utc() av det men med samma dag som idag
-		//Flera punkter skapas därmed, varav alla ligger på idag. Skapa regression utifrån denna
-		//Starttime och endtime för regression blir därmed idag
-
-		let currentDate = moment().toObject()
+		//Spridningsdiagram, se till att dom e tillräckligt utspridda för att visas
 
 		for (let i = 0; i < allLogs.length; i++) {
 
@@ -28,25 +20,18 @@
 			const timeObj=moment.unix(log.time).toObject() 
 			const hourTime=timeObj.hours+(timeObj.minutes/60)+(timeObj.seconds/60/60)
 
-			tmpArray.push([hourTime, log.painLevel])
-
 			logPoints.x.push(hourTime)
 			logPoints.y.push(log.painLevel)
 		}
-		tmpArray.sort((a, b)=> a[0] > b[0] ? 1 :((b[0] > a[0]) ? -1 : 0))
-
-		console.log('log', logPoints);
-		console.log('temp', tmpArray);
 
 		const regression= new PolyReg(logPoints.x, logPoints.y, 4)
-		console.log(regression);
 
 		for (let i=0;  i<24; i = i+ 1) {
 			const yVal=regression.predict(i)
 			regPoints.push([i, yVal])
 			
 		}
-		return {logPoints:tmpArray, regPoints}
+		return regPoints
 
 	}
 
@@ -60,8 +45,7 @@
 
 			let day = moment.unix(log.time).format("YYYY-MM-DD");
 
-			// if (day === currentSimpleDate) {
-			if(true){
+			if (day === currentSimpleDate) {
 				const timeObj=moment.unix(log.time).toObject()
 				todaysData.push([timeObj.hours+(timeObj.minutes/60)+(timeObj.seconds/60/60), log.painLevel])
 			}
@@ -89,8 +73,6 @@
 		//Hämtar datan från firebase
 		const allData = await getAllCurrentLogs()
 		
-		const timeFrame=getTimeFrame()
-		
 		const todaysDatapoints=getTodaysData(allData)
 		const regressionDataPoints=getRegressionPoints(allData)
 		
@@ -98,22 +80,14 @@
 		
 		let config = {
 			series: [
-				// {
-				// 	name: "",
-				// 	// data: regressionDataPoints
-				// 	data: todaysDatapoints
-					
-				// },
 				{
 					name: "",
-					data: regressionDataPoints.regPoints
-					// data: todaysDatapoints
+					data: todaysDatapoints
 					
 				},
 				{
 					name: "",
-					data: regressionDataPoints.logPoints
-					// data: todaysDatapoints
+					data: regressionDataPoints
 					
 				},
 			],
